1fdb685415049fcd4bb20f33fb26f4342ce915be
Annie15N <annie.nelson158@gmail.com>
2025-01-25 17:26:44 +0000

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -5,27 +5,45 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-entry"
-        />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-entry w-100"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
 
       <h1>London</h1>
+
       <ul>
         <li>Wednesday 07.00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="mostly cloudy"
-          />
-          7℃
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="mostly cloudy"
+              className=""
+            />
+            <span className="temperature">7</span>
+            <span className="unit">℃</span>
+          </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
