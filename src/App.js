091ca7e387fb176eeaaeb98de44a139751bdf8e4
Annie15N <annie.nelson158@gmail.com>
2025-01-25 14:05:54 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Annie Nelson and is
-        <a
-          href="https://github.com/Annie15N/react-project-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Annie Nelson and is {""}
+          <a
+            href="https://github.com/Annie15N/react-project-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
