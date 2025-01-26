39eb9aa45d3343c3470f3268e41d13bb970dc88a
Annie15N <annie.nelson158@gmail.com>
2025-01-26 12:01:19 +0000

    Further netlify bugs

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
           <a
             href="https://github.com/Annie15N/react-project-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             GitHub
           </a>
@@ -18,6 +19,7 @@ export default function App() {
           <a
             href="https://gorgeous-paprenjak-1743ac.netlify.app/"
             target="_blank"
+            rel="noreferrer"
           >
             Netlify
           </a>
